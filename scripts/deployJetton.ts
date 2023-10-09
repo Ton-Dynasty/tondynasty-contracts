f3100e67c62d9a397eac3ac65e403936ccbf0946
alan890104 <alan890104@gmail.com>
2023-10-10 01:14:39 +0800

    * fix(deployJetton.ts): update jettonParams values for name, description, symbol, and image * chore(deployJetton.ts): update value for deployment transaction

--- scripts/deployJetton.ts
+++ scripts/deployJetton.ts
@@ -50,17 +50,17 @@ export async function run(provider: NetworkProvider) {
     const deployer = provider.sender();
     console.log('Deploying contract with deployer address', deployer.address);
     const jettonParams = {
-        name: 'Eventon',
-        description: 'This is description of Test Jetton Token in Tact-lang',
-        symbol: 'EVT',
-        image: 'https://raw.githubusercontent.com/Ton-Dynasty/eventon/main/utils/eventon.png',
+        name: 'TonDynasty',
+        description: 'TonDynasty Co-Founder Certificate - Tact',
+        symbol: 'TDT',
+        image: 'https://avatars.githubusercontent.com/u/144251015?s=400&u=a25dfca41bdc6467d9783f5225c93f60e1513630&v=4',
     };
     const onchainContent = buildOnchainMetadata(jettonParams);
     const nFTCollection = provider.open(await ExampleJettonMaster.fromInit(deployer.address!, onchainContent));
     await nFTCollection.send(
         provider.sender(),
         {
-            value: toNano('0.05'),
+            value: toNano('0.03'),
         },
         {
             $$type: 'Deploy',
