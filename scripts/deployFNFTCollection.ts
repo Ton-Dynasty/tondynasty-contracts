681eaffa79619e9855257c852daa1f5301b29aca
alan890104 <alan890104@gmail.com>
2023-10-09 20:53:01 +0800

    feat: complete structure of FNFT

---
+++ scripts/deployFNFTCollection.ts
@@ -0,0 +1,34 @@
+import { beginCell, toNano } from 'ton-core';
+import { FNFTCollection, RoyaltyParams } from '../wrappers/FNFT';
+import { NetworkProvider } from '@ton-community/blueprint';
+
+const OFFCHAIN_TAG = 0x01;
+const BASE_URL = 'https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/';
+
+export async function run(provider: NetworkProvider) {
+    const deployer = provider.sender();
+    console.log('Deploying contract with deployer address', deployer.address);
+    const collectionContent = beginCell().storeInt(OFFCHAIN_TAG, 8).storeStringRefTail(BASE_URL).endCell();
+    const royaltyParams: RoyaltyParams = {
+        $$type: 'RoyaltyParams',
+        numerator: 1n,
+        denominator: 100n,
+        destination: deployer.address!,
+    };
+    const nFTCollection = provider.open(
+        await FNFTCollection.fromInit(deployer.address!, collectionContent, royaltyParams)
+    );
+
+    await nFTCollection.send(
+        provider.sender(),
+        {
+            value: toNano('0.05'),
+        },
+        {
+            $$type: 'Deploy',
+            queryId: 0n,
+        }
+    );
+
+    await provider.waitForDeploy(nFTCollection.address);
+}
