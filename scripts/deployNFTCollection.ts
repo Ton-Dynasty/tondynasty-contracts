5b6aecd5c883ea8a8b524a475b152200eb028672
alan890104 <alan890104@gmail.com>
2023-10-09 13:55:21 +0800

    * feat(nft_example.tact): import "@stdlib/deploy" in nft_example.tact * fix(nft_example.tact): change Mint message to receive "Mint" instead of Mint object * fix(nft_example.tact): change query_id in Transfer body to 0 * fix(nft_example.tact): change new_owner in Transfer body to ctx.sender * fix(nft_example.tact): change custom_payload in Transfer body to random content * fix(nft_example.tact): change forward_amount in Transfer body to 0 * fix(nft_example.tact): change forward_payload in Transfer body to empty cell * fix(nft_example.tact): change content_map key type to uint32 * feat(nft_example.tact): add Deployable extension to ExampleNFTCollection * feat(deployNFTCollection.ts): deploy ExampleNFTCollection contract with deployer address * feat(deployNFTCollection.ts): initialize ExampleNFTCollection with initCollectionContent and

--- scripts/deployNFTCollection.ts
+++ scripts/deployNFTCollection.ts
@@ -1,9 +1,20 @@
-import { toNano } from 'ton-core';
-import { NFTCollection } from '../wrappers/NFTExample';
+import { beginCell, toNano } from 'ton-core';
+import { ExampleNFTCollection, RoyaltyParams } from '../wrappers/NFTExample';
 import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const nFTCollection = provider.open(await NFTCollection.fromInit());
+    const deployer = provider.sender();
+    console.log('Deploying contract with deployer address', deployer.address);
+    const initCollectionContent = beginCell().endCell();
+    const royaltyParams: RoyaltyParams = {
+        $$type: 'RoyaltyParams',
+        numerator: 1n,
+        denominator: 100n,
+        destination: deployer.address!,
+    };
+    const nFTCollection = provider.open(
+        await ExampleNFTCollection.fromInit(deployer.address!, initCollectionContent, royaltyParams)
+    );
 
     await nFTCollection.send(
         provider.sender(),
@@ -17,6 +28,4 @@ export async function run(provider: NetworkProvider) {
     );
 
     await provider.waitForDeploy(nFTCollection.address);
-
-    // run methods on `nFTCollection`
 }
--- tests/NFTExample.spec.ts
+++ tests/NFTExample.spec.ts
@@ -1,18 +1,28 @@
-import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton-community/sandbox';
 import { Cell, beginCell, toNano } from 'ton-core';
-import { ExampleNFTCollection } from '../wrappers/NFTExample';
+import { ExampleNFTCollection, RoyaltyParams } from '../wrappers/NFTExample';
 import '@ton-community/test-utils';
+import { ExampleNFTItem } from '../build/NFTExample/tact_ExampleNFTItem';
 
-describe('NFTCollection', () => {
+describe('NFTExample', () => {
     let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let collectionContent: Cell;
+    let royaltyParams: RoyaltyParams;
     let nftCollection: SandboxContract<ExampleNFTCollection>;
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
-
-        nftCollection = blockchain.openContract(await ExampleNFTCollection.fromInit());
-
-        const deployer = await blockchain.treasury('deployer');
+        deployer = await blockchain.treasury('deployer');
+        collectionContent = beginCell().storeStringRefTail('Hello').endCell();
+        royaltyParams = {
+            $$type: 'RoyaltyParams',
+            numerator: 4n,
+            denominator: 100n,
+            destination: deployer.address,
+        };
+        const initCode = await ExampleNFTCollection.fromInit(deployer.address, collectionContent, royaltyParams);
+        nftCollection = blockchain.openContract(initCode);
 
         const deployResult = await nftCollection.send(
             deployer.getSender(),
@@ -36,5 +46,29 @@ describe('NFTCollection', () => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and nFTCollection are ready to use
+    });
+
+    it('should mint NFT', async () => {
+        const newOwner = await blockchain.treasury('newOwner');
+        const customPayload = beginCell().storeStringRefTail('World').endCell();
+
+        const mintResult = await nftCollection.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.5'),
+            },
+            'Mint'
+        );
+
+        expect(mintResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: nftCollection.address,
+            deploy: false,
+            success: true,
+        });
+
+        const nftItem = blockchain.openContract(
+            await ExampleNFTItem.fromInit(nftCollection.address, 0n, newOwner.address, beginCell().endCell())
+        );
     });
 });
