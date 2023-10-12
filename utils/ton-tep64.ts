0e72d0e6019e31c22b4a47c8b1e7fbc58f2a6480
alan890104 <alan890104@gmail.com>
2023-10-13 01:38:38 +0800

    * feat(ton-tep64.ts): add support for building JettonContent dictionary or NFT item index in buildJettonContent function

--- utils/ton-tep64.ts
+++ utils/ton-tep64.ts
@@ -7,6 +7,13 @@ export const SNAKE_PREFIX = 0x00;
 export const CELL_MAX_SIZE_BYTES = Math.floor((1023 - 8) / 8);
 export const NFT_BASE_URL = 'https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/';
 
+export interface JettonContent {
+    name: string;
+    description: string;
+    symbol: string;
+    image?: string;
+}
+
 function bufferToChunks(buff: Buffer, chunkSize: number) {
     let chunks: Buffer[] = [];
     while (buff.byteLength > 0) {
@@ -37,16 +44,21 @@ const toKey = (key: string) => {
     return BigInt(`0x${sha256_sync(key).toString('hex')}`);
 };
 
-export function buildJettonContent(name: string, description: string, symbol: string, image: string): Cell {
+// data can be either jetton content dict or NFT item index
+export function buildJettonContent(data: JettonContent | bigint): Cell {
     let dict = Dictionary.empty(Dictionary.Keys.BigUint(256), Dictionary.Values.Cell());
-    Object.entries({
-        name,
-        description,
-        symbol,
-        image,
-    }).forEach(([key, value]) => {
-        dict.set(toKey(key), makeSnakeCell(Buffer.from(value, 'utf8')));
-    });
+    if (typeof data === 'bigint') {
+        const name = `F-TonDynasty #${data}`;
+        const description = `${data}`;
+        const symbol = `F-TDT #${data}`;
+        dict.set(toKey('name'), makeSnakeCell(Buffer.from(name, 'utf8')));
+        dict.set(toKey('description'), makeSnakeCell(Buffer.from(description, 'utf8')));
+        dict.set(toKey('symbol'), makeSnakeCell(Buffer.from(symbol, 'utf8')));
+    } else {
+        Object.entries(data).forEach(([key, value]) => {
+            dict.set(toKey(key), makeSnakeCell(Buffer.from(value, 'utf8')));
+        });
+    }
 
     return beginCell().storeInt(ONCHAIN_CONTENT_PREFIX, 8).storeDict(dict).endCell();
 }
