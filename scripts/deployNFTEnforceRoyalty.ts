605d0e1fbf8ccbfb1670b29867691f387869cec4
alan890104 <alan890104@gmail.com>
2023-10-13 16:00:11 +0800

    * feat(deployFNFT.ts): update import path for FNFTCollection to '../wrappers/FNFT_FNFTCollection' * refactor(deployFNFT.ts): update royaltyParams numerator and denominator values * feat(deployFNFT.ts): add buildNFTCollectionContent function import * refactor(deployJetton.ts): update import path for ExampleJettonMaster to '../wrappers/JettonExample_ExampleJettonMaster' * refactor(deployJetton.ts): update buildJettonContent function import * feat(deployJetton.ts): remove unused variables and functions * feat(deployJetton.ts): update jettonParams to jettonContent * feat(deployNFTEnforceRoyalty.ts): add new file * feat(deployNFTEnforceRoyalty.ts): import FNFTCollection from '../wrappers/FNFTEnforce_FNFTCollection' * feat(deployNFTEnforceRoyalty.ts): add buildNFT

--- scripts/deployNFTEnforceRoyalty.ts
+++ scripts/deployNFTEnforceRoyalty.ts
@@ -1,29 +1,25 @@
 import { beginCell, toNano } from 'ton-core';
-import { FNFTCollection, RoyaltyParams } from '../wrappers/FNFT_FNFTCollection';
+import { FNFTCollection, RoyaltyParams } from '../wrappers/FNFTEnforce_FNFTCollection';
 import { NetworkProvider } from '@ton-community/blueprint';
 import { buildNFTCollectionContent } from '../utils/ton-tep64';
-
-const OFFCHAIN_TAG = 0x01;
-const BASE_URL = 'https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/';
 
 export async function run(provider: NetworkProvider) {
     const deployer = provider.sender();
     console.log('Deploying contract with deployer address', deployer.address);
     const collectionContent = buildNFTCollectionContent();
-
     const royaltyParams: RoyaltyParams = {
         $$type: 'RoyaltyParams',
-        numerator: 1n,
-        denominator: 120n,
+        numerator: 2n,
+        denominator: 100n,
         destination: deployer.address!,
     };
     const nftCollection = provider.open(
-        await FNFTCollection.fromInit(deployer.address!, collectionContent, royaltyParams)
+        await FNFTCollection.fromInit(deployer.address!, collectionContent, royaltyParams, deployer.address!)
     );
     await nftCollection.send(
         provider.sender(),
         {
-            value: toNano('0.05'),
+            value: toNano('0.5'),
         },
         {
             $$type: 'Deploy',
