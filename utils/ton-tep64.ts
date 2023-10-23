51e162a31c6fc9bc75ed1d6a0ddcc7309a83e0c7
skyline9981 <skyline.cs12@nycu.edu.tw>
2023-10-23 22:49:54 +0800

    feat: add optional Attribute in ton-tep64.ts

--- utils/ton-tep64.ts
+++ utils/ton-tep64.ts
@@ -8,10 +8,14 @@ export const CELL_MAX_SIZE_BYTES = Math.floor((1023 - 8) / 8);
 export const NFT_BASE_URL = 'https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/';
 
 export interface JettonContent {
+    uri?: string;
     name: string;
     description: string;
     symbol: string;
     image?: string;
+    decimals?: string;
+    amount_style?: string;
+    render_type?: string;
 }
 
 function bufferToChunks(buff: Buffer, chunkSize: number) {
